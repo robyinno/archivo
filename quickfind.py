8838da922ee49f9c8a29bb7dab42f3a29ffb3981
Roberto Innocenti <robyinno@gmail.com>
2013-01-21 02:41:00 +0100

    added change language

--- quickfind.py
+++ quickfind.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 from gi.repository import Gtk, WebKit
 import os, sys
+import locale
+import gettext
+
+APP = 'quickfind'
+DIR = 'locale'
+
+locale.setlocale(locale.LC_ALL,'')
+gettext.bindtextdomain(APP,DIR)
+gettext.textdomain(APP)
+_ = gettext.gettext
 
 UI_FILE='quickfindhome.ui'
 ARCHIVO_DIR='/home/roby/Archivo/'
@@ -23,7 +33,7 @@ class QuickHome:
 	def on_home_clicked(self,button):
 		uri='file://%s%s/archital.htm'%(ARCHIVO_DIR,LANG)
 		self.webview.load_uri(uri)
-		label1=self.builder.get_object('label1')
+		label1=self.builder.get_object('lab_lang_selected')
 		label1.set_text(uri)
 
 	def on_button_clicked(self,button):
@@ -33,11 +43,21 @@ class QuickHome:
 	def on_exit_clicked(self,button):
 		Gtk.main_quit()
 
-	#def on_search_clicked(self,button):
-		# apre una nuova finestra quella di ricerca
+	def lang_changed(self,button):
+		change_language()
 
-	#def on_language_changed(self):
+	def on_search_clicked(self,button):
+		# apre una nuova finestra quella di ricerca
+		Gtk.main_quit() # provvisorio
+
+	def on_lang_changed(self,button,data=None):
 		# cambia la lingua
+		list_store = button.get_model()
+		active_index = button.get_active()
+		active_text = list_store[active_index][0]
+		if active_text != None:
+			label1=self.builder.get_object('lab_lang_selected')
+			label1.set_text(active_text)
 
 def main():
 	app = QuickHome()
